ba9c5caaaf9df4a498736230b4eb1b708b86b6ed
João Pedro <nmjoaopedro22@gmail.com>
2024-06-06 09:38:49 -0300

    cria classe Position

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,10 @@
 package app;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java World!");
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
---
+++ src/main/java/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+    private int row;
+    private int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d, %d", row, col);
+    }
+}
